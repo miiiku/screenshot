2a14bd6a7bd63528209b149f9fd3adc875f11102
Sukoshi <guanquanhong@163.com>
2023-02-08 15:35:16 +0800

    feat(update): 使用cdn的json数据，添加一点点响应式样式

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,31 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import Header from './components/Header.vue';
 import Section from './components/Section.vue';
 
-const datas = [
-  {
-    title: "DARKER THAN BLACK -黑之契约者-",
-    images: [
-      "https://qiniu.sukoshi.xyz/screenshot/darker-than-black/IMG_1070.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/darker-than-black/IMG_1071.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/darker-than-black/IMG_1078.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/darker-than-black/IMG_1079.jpeg@webp",
-    ],
-  },
-  {
-    title: "比宇宙更远的地方",
-    images: [
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1335.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1338.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1339.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1343.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1348.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1351.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1355.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/farther-than-the-universe/IMG_1360.jpeg@webp",
-    ],
-  },
-  {
-    title: "野良神",
-    images: [
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1174.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1207.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1209.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1176.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1208.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1201.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1205.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1203.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/noragami/IMG_1206.jpeg@webp",
-    ],
-  },
-  {
-    title: "WONDER EGG PRIORITY(奇蛋物语)",
-    images: [
-      "https://qiniu.sukoshi.xyz/screenshot/wonder-egg-priority/IMG_1014.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/wonder-egg-priority/IMG_1084.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/wonder-egg-priority/IMG_1083.jpeg@webp",
-      "https://qiniu.sukoshi.xyz/screenshot/wonder-egg-priority/IMG_1217.jpeg@webp",
-    ],
-  },
-  {
-    title: "金色时光/青春纪行",
-    images: [
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3263.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3264.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3266.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3267.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3268.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3269.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3270.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3271.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3272.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3273.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3276.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3277.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3278.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3280.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3282.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3283.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3284.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3285.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3286.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3287.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3291.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3294.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3295.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3296.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3297.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3299.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3300.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3301.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3302.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3303.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3304.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3305.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3312.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3313.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3314.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3315.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3316.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3317.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3318.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3319.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3321.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3322.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3323.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3324.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3325.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3326.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3327.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3328.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3329.jpeg",
-      "https://qiniu.sukoshi.xyz/screenshot/golden-time/IMG_3330.jpeg",
-    ]
-  }
-]
+interface Screenshot {
+  title: string,
+  images: string[],
+}
 
-const titles = computed(() => {
-  return datas.map(({ title }: { title: string }) => title);
-})
+const dataList = ref<Array<Screenshot>>([])
 
-onMounted(() => {
+const fetchList = (callback?: () => void) => {
+  fetch("https://qiniu.sukoshi.xyz/screenshot/screenshot.json").then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject("获取数据失败")
+  }).then(data => {
+    dataList.value = data;
+    callback && callback();
+  })
+  .catch(error => {
+    alert(error || "网络异常");
+  });
+}
+
+const initObserver = () => {
   const headerH = 60;
   const titleContainer = document.querySelector(".title-list") as HTMLElement;
   const titleEls = Array.from(document.querySelectorAll(".section-item--title"));
@@ -144,9 +59,20 @@ onMounted(() => {
      */
     titleContainer.style.transform = `translate3d(0, ${firstVisibleIndex * -100}%, 0)`;
   },
+
   options);
 
   titleEls.forEach((title) => titleObserver.observe(title));
+}
+
+const titles = computed(() => {
+  return dataList.value.map(({ title }: { title: string }) => title);
+})
+
+onMounted(() => {
+  fetchList(() => {
+    setTimeout(initObserver, 200);
+  });
 })
 </script>
 
@@ -154,7 +80,7 @@ onMounted(() => {
   <Header :titles="titles" />
 
   <Section
-    v-for="(section, index) in datas"
+    v-for="(section, index) in dataList"
     :key="index"
     :index="index"
     :title="section.title"
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -7,44 +7,12 @@ const props = defineProps<{
 </script>
 
 <template>
-  <section class="section-item">
-    <div class="section-item--title" :data-index="index"># {{ props.title }}</div>
-    <div class="section-item--screenshots">
-      <div class="screenshot-block" v-for="(url, index) in props.list" :key="index">
-        <img :data-src="url" :src="url" loading="lazy" :alt="props.title" />
+  <section class="section-item p-5">
+    <div class="section-item--title mb-5 font-bold text-lg md:text-lg lg:text-xl xl:text-2xl" :data-index="index"># {{ props.title }}</div>
+    <div class="section-item--screenshots grid gap-3 grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <div class="screenshot-block rounded-lg p-2 border border-gray-300" v-for="(url, index) in props.list" :key="index">
+        <img class="block rounded object-cover aspect-video" :data-src="url" :src="url" loading="lazy" :alt="props.title" />
       </div>
     </div>
   </section>
 </template>
-
-<style scoped>
-.section-item {
-  padding: 20px;
-}
-
-.section-item .section-item--title {
-  font-size: 28px;
-  font-weight: bolder;
-  margin-bottom: 20px;
-}
-
-.section-item .section-item--screenshots {
-  display: grid;
-  grid-template-columns: repeat(3, auto);
-  gap: 12px;
-}
-
-.section-item .section-item--screenshots .screenshot-block {
-  border-radius: 8px;
-  padding: 8px;
-  border: 1px solid #E1E1E1;
-}
-
-.section-item .section-item--screenshots .screenshot-block img {
-  display: block;
-  width: 100%;
-  border-radius: 4px;
-  object-fit: cover;
-  aspect-ratio: 16/9;
-}
-</style>
